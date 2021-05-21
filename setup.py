c395903d9dd71a06dc3c9fce9c2f44b9640d26fb
Park Sung Min <sm.park@coint.co.kr>
2021-05-21 16:49:05 +0900

    bump version 0.0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='coint-paginatify-sqlalchemy',
-    version='0.0.2',
+    version='0.0.3',
     packages=['paginatify_sqlalchemy', 'paginatify_sqlalchemy'],
     zip_safe=False,
     install_requires=['sqlalchemy>=1.0.11']
